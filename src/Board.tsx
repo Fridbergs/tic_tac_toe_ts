20e957935ae316e73f5bf49b988f8cb22f83a772
Fridbergs <144354526+Fridbergs@users.noreply.github.com>
2024-04-24 10:45:47 +0200

    Second commit

---
+++ src/Board.tsx
@@ -0,0 +1,75 @@
+import { useState } from "react";
+import Square from "./Square";
+
+function Board() {
+  const [squares, setSquares] = useState<Array<string | null>>(
+    Array(9).fill(null)
+  );
+  const [xIsNext, setXIsNext] = useState<boolean>(true);
+
+  const handleClick = (index: number) => {
+    if (squares[index] || calculateWinner(squares)) {
+      return;
+    }
+    const newSquares = squares.slice();
+    newSquares[index] = xIsNext ? "X" : "O";
+    setSquares(newSquares);
+    setXIsNext(!xIsNext);
+  };
+
+  const renderSquare = (index: number) => {
+    return <Square value={squares[index]} onClick={() => handleClick(index)} />;
+  };
+
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = `Winner: ${winner}`;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
+
+  return (
+    <div>
+      <div className="status">{status}</div>
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </div>
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+      <button onClick={() => setSquares(Array(9).fill(null))}>Restart</button>
+    </div>
+  );
+}
+
+function calculateWinner(squares: Array<string | null>): string | null {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+export default Board;
